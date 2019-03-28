989ee76991d33541346a921e3d11e8ee3693b62e
Mrigank Shekhar <mrigshe102@gmail.com>
2019-03-28 13:15:57 +0000

    Star Programs

--- whyNotJavaPrograms/src/whyNotJavaPrograms/ClassA.java
+++ whyNotJavaPrograms/src/whyNotJavaPrograms/ClassA.java
@@ -1,5 +1,26 @@
 package whyNotJavaPrograms;
+/**
+* @author      Mrigank Shekhar <mrigankshekhar@icloud.com>
+* @version     1.0          (current version number of program)
+* @since       1.0          (the version of the package this class was first added to)
+*/
+import java.util.Scanner;
 
 public class ClassA {
-
+public static void main(String[] args) {
+	/*
+	 * Printing triangle of stars
+	 */
+	System.out.print("How many stars you want? __");
+	Scanner scan = new Scanner(System.in);
+	int a = scan.nextInt();
+		for(int i=1;i<=a;i++)
+		{
+			for (int j=0; j<i;j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
 }
---
+++ whyNotJavaPrograms/src/whyNotJavaPrograms/ClassB.java
@@ -0,0 +1,5 @@
+package whyNotJavaPrograms;
+
+public class ClassB {
+
+}
