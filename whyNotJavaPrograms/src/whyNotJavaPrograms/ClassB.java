8ea0322fa8414f36219ad83c48596a4bb02dcbe2
Mrigank Shekhar <mrigshe102@gmail.com>
2019-03-28 14:09:44 +0000

    Inverted Trangle

--- whyNotJavaPrograms/src/whyNotJavaPrograms/ClassB.java
+++ whyNotJavaPrograms/src/whyNotJavaPrograms/ClassB.java
@@ -1,5 +1,27 @@
 package whyNotJavaPrograms;
 
-public class ClassB {
+import java.util.Scanner;
 
+/**
+* @author      Mrigank Shekhar <mrigankshekhar@icloud.com>
+* @version     1.0          (current version number of program)
+* @since       1.0          (the version of the package this class was first added to)
+*/
+public class ClassB {
+public static void main(String[] args) {
+	/*
+	 * Printing inverted triangle of stars
+	 */
+	System.out.print("How many stars you want? __");
+	Scanner scan = new Scanner(System.in);
+	int a = scan.nextInt();
+	for (int i=0; i<a; i++)
+	{
+		for (int j=a;j>i; j--)
+		{
+			System.out.print("* ");
+		}
+		System.out.println();
+	}
+}
 }
